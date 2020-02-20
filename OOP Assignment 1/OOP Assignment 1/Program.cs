a178786e52ddd0faecc7a3a6eca22e5fdee5e4db
Ewan Jackson Corbett <ewanjacksoncorbett123@gmail.com>
2020-02-20 16:13:24 +0000

    Country Class Blueprint Created
    Created the bass blueprint for the EU countries to be based off

--- OOP Assignment 1/OOP Assignment 1/Program.cs
+++ OOP Assignment 1/OOP Assignment 1/Program.cs
@@ -6,7 +6,45 @@ namespace OOP_Assignment_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Country Austria = new Country("Austria", (1.98f));
+            Console.WriteLine(Austria.CountryName);
+            Console.WriteLine(Austria.CountryPop);
+
+
         }
     }
+
+
+
+    class Country
+    {
+        private string _CountryName;
+        private float _CountryPop;
+
+        public string CountryName
+        {
+            get { return _CountryName; }
+            set { _CountryName = value; }
+        }
+
+        public float CountryPop
+        {
+            get { return _CountryPop; }
+            set { _CountryPop = value; }
+
+        }
+
+
+        //country constructor
+        public Country(string cName, float pop)
+        {
+            _CountryName = cName;
+            _CountryPop = pop;
+        }
+        
+    }
+
+
 }
+
+    
